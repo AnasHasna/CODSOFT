a9d429ee6e19c93f0aa8fbe7c4c0d1fb55c86ade
Anas Hasna <hasnaanas230@gmail.com>
2023-11-23 21:11:07 +0100

    finished user router commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,4 +38,102 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser };
+/**
+ * @desc    login user
+ * @route   GET /api/users/login
+ * @access  Public
+ */
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    email,
+  });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      adress: user.adress,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+/**
+ * @desc    reset password
+ * @route   POST /api/users/profile
+ * @access  Public
+ */
+const resetPassword = asyncHandler(async (req, res) => {
+  const { email, newPassword } = req.body;
+  try {
+    const user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.password = newPassword;
+    await user.save();
+    res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+/**
+ * @desc    update user profile
+ * @route   PUT /api/users/profile
+ * @access  Private
+ */
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.fullName = req.body.fullName || user.fullName;
+    user.email = req.body.email || user.email;
+    user.phoneNumber = req.body.phoneNumber || user.phoneNumber;
+    user.adress = req.body.adress || user.adress;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      fullName: updatedUser.fullName,
+      email: updatedUser.email,
+      phoneNumber: updatedUser.phoneNumber,
+      adress: updatedUser.adress,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+/**
+ * @desc    logout user
+ * @route   GET /api/users/logout
+ * @access  Public
+ */
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
+  res.status(200).json({
+    message: "User logged out",
+  });
+});
+
+export {
+  registerUser,
+  loginUser,
+  resetPassword,
+  updateUserProfile,
+  logoutUser,
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  resetPassword,
+  updateUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+
+router.route("/profile").put(protect, updateUserProfile).post(resetPassword);
 
 export default router;
